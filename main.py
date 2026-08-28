67fcc51eb112f35ce4a8b22c433d88f3386510c9
Roopanshi-Marwaha <roopanshimarwaha@gmail.com>
2026-08-29 01:17:56 +0530

    Prepare app for deployment - add gunicorn, remove debug mode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 data = pd.read_csv('Cleaned_data.csv')
@@ -29,4 +30,4 @@ def predict():
     return str(np.round(prediction,2))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
